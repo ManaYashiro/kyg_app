97d910ed486a8f5f33533947906845b153e07de8
rnanayashiro <rnanayashiro@gmail.com>
2024-11-08 17:43:12 +0900

    mypage etc

---
+++ resources/views/frequentlyAskedQuestions.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Frequently Asked Questions') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        よくある質問
+    </div>
+    <div>
+        過去に多かったお問合せについて回答を掲載しています。
+    </div>
+    <div>
+    </div>
+</x-app-layout>
