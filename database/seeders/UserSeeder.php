1e98fa4ac6847a9a27251ac070fc7315593cbaa0
kpebron <kpebron@gmail.com>
2024-11-08 17:21:57 +0900

    feat: user 登録とシーダーデータ

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create(
+            array_merge($this->otherData(true), [
+                'name' => 'admin',
+                'furigana_name' => 'admin',
+                'role' => User::ADMIN,
+                'email' => 'admin@admin.admin',
+                'password' => 'adminp@ssword1234',
+            ])
+        );
+        User::factory()->create(
+            array_merge($this->otherData(false), [
+                'name' => 'user',
+                'furigana_name' => 'user',
+                'role' => User::USER,
+                'email' => 'user@user.user',
+                'password' => 'p@ssword1234',
+            ])
+        );
+        for ($i = 0; $i < 10; $i++) {
+            User::factory()->create(
+                array_merge($this->otherData(false), [
+                    'name' => fake()->name(),
+                    'furigana_name' => fake()->name(),
+                    'role' => User::USER,
+                    'email' => fake()->email(),
+                    'password' => 'p@ssword1234',
+                ])
+            );
+        }
+    }
+
+    public function otherData(bool $isAdmin = false): array
+    {
+        return [
+            'phone_number' => '000-0000-0000',
+            'post_code' => 5320011,
+            'address' => '西中島南方',
+            'building' => 'NLC Building',
+            'preferred_contact_time' => $isAdmin ? null : fake()->time('H:i'),
+            'is_newsletter_subscription' => $isAdmin ? FALSE : rand(0, 1),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,20 @@ Route::get('/mypage', function () {
     return view('mypage');
 })->middleware(['auth', 'verified'])->name('mypage');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
+    Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
