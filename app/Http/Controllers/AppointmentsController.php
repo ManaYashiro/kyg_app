1824e2442aa07abe17b65ad664bf2cb07f9d7ebe
100seaocean <teruyuki.doumi@nexus-jp.co.jp>
2024-11-12 13:06:36 +0900

    登録処理

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -14,29 +14,81 @@ class  AppointmentsController extends Controller
 
     public function store(Request $request)
     {
-        // バリデーション
-        $validatedData = $request->validate([
+        //車両名を判定のために受け取る
+        $vehicle_names = $request->input('vehicle_name',[]);
+
+        //1台目が入力されて、2台目が未入力の場合
+        // if (isset($vehicle_names[0]) && (empty($vehicle_names[1]))) {
+        //     // 1台目だけのバリデーション
+        //     $validatedData = $request->validate([
+        //         'reservation_datetime' => 'nullable|date',
+        //         'vehicle_name.0' => 'required|string|max:255',
+        //         'registration_number.0' => 'required|string|max:255',
+        //         'vehicle_type.0' => 'required|string|max:255',
+        //         'license_plate.0' => 'required|string|max:255',
+        //         'inspection_due_date.0' => 'required|date',
+        //         'additional_services.0' => 'array|nullable',
+        //         'past_service_history' => 'required|string',
+        //         'message' => 'nullable|string|max:255',
+        //     ]);
+        // } else {
+        //     // 両方に対するバリデーション
+        //     $validatedData = $request->validate([
+        //         'reservation_datetime' => 'nullable|date',
+        //         'vehicle_name.*' => 'required|string|max:255',
+        //         'registration_number.*' => 'required|string|max:255',
+        //         'vehicle_type.*' => 'required|string|max:255',
+        //         'license_plate.*' => 'required|string|max:255',
+        //         'inspection_due_date.*' => 'required|date',
+        //         'additional_services.*' => 'array|nullable',
+        //         'past_service_history' => 'required|string',
+        //         'message' => 'nullable|string|max:255',
+        //     ]);
+        // }
+        // バリデーションルール初期化
+        $validationRules = [
             'reservation_datetime' => 'nullable|date',
-            'vehicle_name' => 'required|string|max:255',
-            'registration_number' => 'required|string|max:255',
-            'vehicle_type' => 'required|string|max:255',
-            'license_plate' => 'required|string|max:255',
-            'inspection_due_date' => 'required|date',
-            'additional_services' => 'array|nullable',
             'past_service_history' => 'required|string',
             'message' => 'nullable|string|max:255',
-        ]);
+        ];
 
-        // 仮でuser_idを設定 (例: 1 を仮のIDとして使用)
-        $validatedData['user_id'] = 1;
-        // sort_numberを手動で設定 (仮に1を設定)
-        $validatedData['sort_number'] = 1;
-        //カンマ区切りで文字列に変換
-        if(isset($request->additional_services)){
-            $validatedData['additional_services'] = implode(",",$request->additional_services);
+        foreach ($vehicle_names as $index => $vehicle_name) {
+            if (!empty($vehicle_name)) {
+                // 車両名が入力されている場合にバリデーションルールを追加
+                $validationRules["vehicle_name.$index"] = 'required|string|max:255';
+                $validationRules["registration_number.$index"] = 'required|string|max:255';
+                $validationRules["vehicle_type.$index"] = 'required|string|max:255';
+                $validationRules["license_plate.$index"] = 'required|string|max:255';
+                $validationRules["inspection_due_date.$index"] = 'required|date';
+                $validationRules["additional_services.$index"] = 'array|nullable';
+            }
         }
-        // 新しいレコードを作成
-        Appointments::create($validatedData);
+
+        $validatedData = $request->validate($validationRules);
+
+        $userId = 1; // 仮のユーザーID
+        $sortNumber = 1; // 仮のソート番号
+
+        // 各車両のデータをループして保存
+        foreach ($validatedData['vehicle_name'] as $index => $vehicleName) {
+            $data = [
+                'reservation_datetime' => $validatedData['reservation_datetime'],
+                'vehicle_name' => $vehicleName,
+                'registration_number' => $validatedData['registration_number'][$index],
+                'vehicle_type' => $validatedData['vehicle_type'][$index],
+                'license_plate' => $validatedData['license_plate'][$index],
+                'inspection_due_date' => $validatedData['inspection_due_date'][$index],
+                'additional_services' => isset($validatedData['additional_services'][$index])
+                ? implode(",", $validatedData['additional_services'][$index])
+                : null,
+                'past_service_history' => $validatedData['past_service_history'],
+                'message' => $validatedData['message'],
+                'user_id' => $userId,
+                'sort_number' => $sortNumber,
+            ];
+            // 新しいレコードを作成
+            Appointments::create($data);
+        }
         return redirect()->route('appointments.index')->with('success', '車検予約が保存されました。');
     }
 
--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -17,6 +17,62 @@
                             <x-input-label for="reservation_datetime" value="予約日時" />
                             <x-text-input id="reservation_datetime" class="block mt-1 w-full" name='reservation_datetime'/>
                         </div>
+                        @for ($i = 0; $i < 2; $i++)
+                            <div class="mt-4">
+                                <x-input-label for="vehicle_name_{{ $i }}" value="車両名" />
+                                <x-text-input id="vehicle_name_{{ $i }}" class="block mt-1 w-full" name="vehicle_name[]"
+                                            :value="old('vehicle_name.' . $i)" />
+                                <x-input-error :messages="$errors->get('vehicle_name.'.$i)" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="registration_number_{{ $i }}" value="車両番号登録" />
+                                <x-text-input id="registration_number_{{ $i }}" class="block mt-1 w-full" name="registration_number[]"
+                                            :value="old('registration_number.' . $i)" />
+                                <x-input-error :messages="$errors->get('registration_number.'.$i)" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="vehicle_type_{{ $i }}" value="車両タイプ" />
+                                <x-select id="vehicle_type_{{ $i }}" class="block mt-1 w-full" name="vehicle_type[]"
+                                        :options="[
+                                            '' => '選択して下さい',
+                                            '1' => 'セダン',
+                                            '2' => 'SUV',
+                                            '3' => 'ワゴン',
+                                            '4' => 'トラック',
+                                            '5' => 'その他',
+                                        ]" />
+                                <x-input-error :messages="$errors->get('vehicle_type.'.$i)" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="license_plate_{{ $i }}" value="ナンバープレート" />
+                                <x-text-input id="license_plate_{{ $i }}" class="block mt-1 w-full" name="license_plate[]"
+                                            :value="old('license_plate.' . $i)" />
+                                <x-input-error :messages="$errors->get('license_plate.'.$i)" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="inspection_due_date_{{ $i }}" value="車両満了日" />
+                                <x-text-input id="inspection_due_date_{{ $i }}" class="block mt-1 w-full" type="date" name="inspection_due_date[]"
+                                            :value="old('inspection_due_date.' . $i)" />
+                                <x-input-error :messages="$errors->get('inspection_due_date.'.$i)" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-input-label for="additional_services_{{ $i }}" value="追加装備" />
+                                <x-checkbox name="additional_services[{{ $i }}][]" value="エンジンオイル交換" label="エンジンオイル交換" :checked="old('additional_services.' . $i, false)" :disabled="false" class="mt-2" />
+                                <x-checkbox name="additional_services[{{ $i }}][]" value="タイヤローテション[前⇔後]" label="タイヤローテション[前⇔後]" :checked="old('additional_services.' . $i, false)" :disabled="false" class="mt-2" />
+                                <x-checkbox name="additional_services[{{ $i }}][]" value="タイヤ付替[夏⇔冬シーズンチェンジ]" label="タイヤ付替[夏⇔冬シーズンチェンジ" :checked="old('additional_services.' . $i, false)" :disabled="false" class="mt-2" />
+                            </div>
+                        @endfor
+
+                        {{-- <div class="mt-4">
+                            <x-input-label for="vehicle_name" value="車検予約" />
+                            <x-input-label for="reservation_datetime" value="予約日時" />
+                            <x-text-input id="reservation_datetime" class="block mt-1 w-full" name='reservation_datetime'/>
+                        </div>
                         <!-- 車両1台目 -->
                         <div class="mt-4">
                             <x-input-label for="vehicle_name" value="車両名" />
@@ -60,87 +116,6 @@
                             <x-checkbox name="additional_services[]" value="エンジンオイル交換" label="エンジンオイル交換" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
                             <x-checkbox name="additional_services[]" value="タイヤローテション[前⇔後]" label="タイヤローテション[前⇔後]" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
                             <x-checkbox name="additional_services[]" value="タイヤ付替[夏⇔冬シーズンチェンジ]" label="タイヤ付替[夏⇔冬シーズンチェンジ]" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
-                        </div>
-
-                        <!-- 車両2台目 -->
-                        {{-- <div class="mt-4">
-                            <x-input-label for="vehicle_name" value="車両名" />
-                            <x-text-input id="vehicle_name" class="block mt-1 w-full" name='vehicle_name'
-                            :value="old('vehicle_name')"/>
-                            <x-input-error :messages="$errors->get('vehicle_name')" class="mt-2" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="registration_number" value="車両番号登録" />
-                            <x-text-input id="registration_number" class="block mt-1 w-full" name='registration_number'
-                            :value="old('registration_number')"/>
-                            <x-input-error :messages="$errors->get('registration_number')" class="mt-2" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="vehicle_type" value="車両タイプ" />
-                            <x-select id="vehicle_type" class="block mt-1 w-full" name="vehicle_type"
-                            :options="[
-                                '' => '選択して下さい',
-                                '1' => 'セダン',
-                                '2' => 'SUV',
-                                '3' => 'ワゴン',
-                                '4' => 'トラック',
-                                '5' => 'その他',
-                            ]"/>
-                            <x-input-error :messages="$errors->get('vehicle_type')" class="mt-2" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="license_plate" value="ナンバープレート" />
-                            <x-text-input id="license_plate" class="block mt-1 w-full" name='license_plate'
-                            :value="old('license_plate')"/>
-                            <x-input-error :messages="$errors->get('license_plate')" class="mt-2" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="inspection_due_date" value="車両満了日" />
-                            <x-text-input  id="inspection_due_date" class="block mt-1 w-full" type="date" name='inspection_due_date'
-                            :value="old('inspection_due_date')"/>
-                            <x-input-error :messages="$errors->get('inspection_due_date')" class="mt-2" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="additional_services" value="追加装備" />
-                            <x-checkbox name="additional_services[]" value="エンジンオイル交換" label="エンジンオイル交換" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
-                            <x-checkbox name="additional_services[]" value="タイヤローテション[前⇔後]" label="タイヤローテション[前⇔後]" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
-                            <x-checkbox name="additional_services[]" value="タイヤ付替[夏⇔冬シーズンチェンジ]" label="タイヤ付替[夏⇔冬シーズンチェンジ]" :checked="old('additional_services', false)" :disabled="false" class="mt-2" />
-                        </div> --}}
-
-                        <!-- 車両3台目 -->
-                        {{-- <div class="mt-4">
-                            <x-input-label for="vehicle_name" value="車両名" />
-                            <x-text-input id="vehicle_name" class="block mt-1 w-full" name='vehicle_name'/>
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="vehicle_name_registration" value="車両番号登録" />
-                            <x-text-input id="vehicle_name_registration" class="block mt-1 w-full" name='vehicle_name_registration'/>
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="vehicle_type" value="車両タイプ" />
-                            <x-select id="vehicle_type" class="block mt-1 w-full" name="vehicle_type"
-                            :options="[
-                                '' => '選択して下さい',
-                                '1' => 'セダン',
-                                '2' => 'SUV',
-                                '3' => 'ワゴン',
-                                '4' => 'トラック',
-                                '5' => 'その他',
-                            ]" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="number_plate" value="ナンバープレート" />
-                            <x-text-input id="number_plate" class="block mt-1 w-full" name='number_plate'/>
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="vehicle_expiration_date" value="車両満了日" />
-                            <x-text-input id="vehicle_expiration_date" class="block mt-1 w-full" name='vehicle_expiration_date'/>
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="extra_equipment" value="追加装備" />
-                            <div><label><input type="checkbox" name="extra_equipment" value="1">エンジンオイル交換</label></div>
-                            <div><label><input type="checkbox" name="extra_equipment" value="2">タイヤローテション[前⇔後]</label></div>
-                            <div><label><input type="checkbox" name="extra_equipment" value="3">タイヤ付替[夏⇔冬シーズンチェンジ]</label></div>
                         </div> --}}
 
                         <!-- 過去利用履歴 -->
