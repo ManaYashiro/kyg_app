0b99bde6cf1ac93c2277f34e44e9daac92071295
kpebron <kpebron@gmail.com>
2024-11-15 17:59:23 +0900

    fix jquery

--- resources/js/modules/base.js
+++ resources/js/modules/base.js
@@ -1,125 +1,127 @@
-function navi() {
-    function getThemeFromLocalStorage() {
-        // if user already changed the theme, use it
-        if (window.localStorage.getItem("dark")) {
-            return JSON.parse(window.localStorage.getItem("dark"));
+$(document).ready(function () {
+    function navi() {
+        function getThemeFromLocalStorage() {
+            // if user already changed the theme, use it
+            if (window.localStorage.getItem("dark")) {
+                return JSON.parse(window.localStorage.getItem("dark"));
+            }
+
+            // else return their preferences
+            return (
+                !!window.matchMedia &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches
+            );
         }
 
-        // else return their preferences
-        return (
-            !!window.matchMedia &&
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-        );
-    }
+        function setThemeToLocalStorage(value) {
+            window.localStorage.setItem("dark", value);
+        }
 
-    function setThemeToLocalStorage(value) {
-        window.localStorage.setItem("dark", value);
-    }
-
-    return {
-        dark: getThemeFromLocalStorage(),
-        toggleTheme() {
-            this.dark = !this.dark;
-            setThemeToLocalStorage(this.dark);
-        },
-        isSideMenuOpen: false,
-        toggleSideMenu() {
-            this.isSideMenuOpen = !this.isSideMenuOpen;
-        },
-        closeSideMenu() {
-            this.isSideMenuOpen = false;
-        },
-        isNotificationsMenuOpen: false,
-        toggleNotificationsMenu() {
-            this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen;
-        },
-        closeNotificationsMenu() {
-            this.isNotificationsMenuOpen = false;
-        },
-        isProfileMenuOpen: false,
-        isProfileMenuToggled: false,
-        toggleProfileMenu() {
-            this.isProfileMenuOpen = !this.isProfileMenuOpen;
-            this.isProfileMenuToggled = true;
-        },
-        closeProfileMenu() {
-            this.isProfileMenuOpen = false;
-            this.isProfileMenuToggled = false;
-        },
-        shouldCloseProfileMenu() {
-            if (this.isProfileMenuToggled) {
+        return {
+            dark: getThemeFromLocalStorage(),
+            toggleTheme() {
+                this.dark = !this.dark;
+                setThemeToLocalStorage(this.dark);
+            },
+            isSideMenuOpen: false,
+            toggleSideMenu() {
+                this.isSideMenuOpen = !this.isSideMenuOpen;
+            },
+            closeSideMenu() {
+                this.isSideMenuOpen = false;
+            },
+            isNotificationsMenuOpen: false,
+            toggleNotificationsMenu() {
+                this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen;
+            },
+            closeNotificationsMenu() {
+                this.isNotificationsMenuOpen = false;
+            },
+            isProfileMenuOpen: false,
+            isProfileMenuToggled: false,
+            toggleProfileMenu() {
+                this.isProfileMenuOpen = !this.isProfileMenuOpen;
+                this.isProfileMenuToggled = true;
+            },
+            closeProfileMenu() {
+                this.isProfileMenuOpen = false;
                 this.isProfileMenuToggled = false;
-            } else {
-                this.closeProfileMenu();
-            }
-        },
-        isPagesMenuOpen: false,
-        togglePagesMenu() {
-            this.isPagesMenuOpen = !this.isPagesMenuOpen;
-        },
-        // Modal
-        isModalOpen: false,
-        trapCleanup: null,
-        openModal() {
-            this.isModalOpen = true;
-            this.trapCleanup = focusTrap(document.querySelector("#modal"));
-        },
-        closeModal() {
-            this.isModalOpen = false;
-            this.trapCleanup();
-        },
-    };
-}
+            },
+            shouldCloseProfileMenu() {
+                if (this.isProfileMenuToggled) {
+                    this.isProfileMenuToggled = false;
+                } else {
+                    this.closeProfileMenu();
+                }
+            },
+            isPagesMenuOpen: false,
+            togglePagesMenu() {
+                this.isPagesMenuOpen = !this.isPagesMenuOpen;
+            },
+            // Modal
+            isModalOpen: false,
+            trapCleanup: null,
+            openModal() {
+                this.isModalOpen = true;
+                this.trapCleanup = focusTrap(document.querySelector("#modal"));
+            },
+            closeModal() {
+                this.isModalOpen = false;
+                this.trapCleanup();
+            },
+        };
+    }
 
-// 削除アラートメッセージ
-$(".delete--model").on("click", function (e) {
-    e.preventDefault();
+    // 削除アラートメッセージ
+    $(".delete--model").on("click", function (e) {
+        e.preventDefault();
 
-    var deleteModel = $(this).closest(".form-delete--model");
-    let modelTitle = deleteModel.data("title");
+        var deleteModel = $(this).closest(".form-delete--model");
+        let modelTitle = deleteModel.data("title");
 
-    Swal.fire({
-        title: "「" + modelTitle + "」を削除してもよろしいですか?",
-        text: "これを元に戻すことはできません！",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "はい",
-        cancelButtonText: "いいえ",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: deleteModel.attr("action"),
-                method: deleteModel.attr("method"),
-                data: deleteModel.serialize(),
-                success: function (response) {
-                    if (response.success) {
-                        Swal.fire({
-                            title: "削除しました！",
-                            text: response.message,
-                            icon: "success",
-                        }).then(() => {
-                            window.location.href = response.redirectUrl;
-                        });
-                    } else {
-                        // Show error message if deletion fails
+        Swal.fire({
+            title: "「" + modelTitle + "」を削除してもよろしいですか?",
+            text: "これを元に戻すことはできません！",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "はい",
+            cancelButtonText: "いいえ",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: deleteModel.attr("action"),
+                    method: deleteModel.attr("method"),
+                    data: deleteModel.serialize(),
+                    success: function (response) {
+                        if (response.success) {
+                            Swal.fire({
+                                title: "削除しました！",
+                                text: response.message,
+                                icon: "success",
+                            }).then(() => {
+                                window.location.href = response.redirectUrl;
+                            });
+                        } else {
+                            // Show error message if deletion fails
+                            Swal.fire({
+                                title: "エラー！",
+                                text: "削除に失敗しました。再試行してください。",
+                                icon: "error",
+                            });
+                        }
+                    },
+                    error: function () {
+                        // Handle any AJAX errors
                         Swal.fire({
                             title: "エラー！",
-                            text: "削除に失敗しました。再試行してください。",
+                            text: "サーバーへのリクエストに失敗しました。",
                             icon: "error",
                         });
-                    }
-                },
-                error: function () {
-                    // Handle any AJAX errors
-                    Swal.fire({
-                        title: "エラー！",
-                        text: "サーバーへのリクエストに失敗しました。",
-                        icon: "error",
-                    });
-                },
-            });
-        }
+                    },
+                });
+            }
+        });
     });
 });
--- resources/js/postalCodeSearch.js
+++ resources/js/postalCodeSearch.js
@@ -1,25 +1,31 @@
-$(document).ready(function() {
-    $('#search-postcode').on('click', function() {
-        var postCode = $('#post_code').val().replace(/[^0-9]/g, ''); // 郵便番号の数字部分を取り出す
+$(document).ready(function () {
+    $("#search-postcode").on("click", function () {
+        var postCode = $("#post_code")
+            .val()
+            .replace(/[^0-9]/g, ""); // 郵便番号の数字部分を取り出す
 
         if (postCode.length === 7) {
             // zipcloud APIを使って郵便番号から住所を取得
             $.ajax({
-                url: 'https://zipcloud.ibsnet.co.jp/api/search',
+                url: "https://zipcloud.ibsnet.co.jp/api/search",
                 data: { zipcode: postCode },
-                dataType: 'json',
-                success: function(data) {
+                dataType: "json",
+                success: function (data) {
                     if (data.results) {
                         var address = data.results[0];
                         // 住所を挿入する
-                        $('#address').val(address.address1 + address.address2 + address.address3);
+                        $("#address").val(
+                            address.address1 +
+                                address.address2 +
+                                address.address3
+                        );
                     } else {
                         alert("住所が見つかりませんでした。");
                     }
                 },
-                error: function() {
+                error: function () {
                     alert("郵便番号の検索に失敗しました。");
-                }
+                },
             });
         } else {
             alert("有効な郵便番号を入力してください。");
