4ca9d1e335c785e0f6978abd71e80aa95568ec07
kpebron <kpebron@gmail.com>
2024-12-17 14:13:34 +0900

    logout only

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -104,6 +104,7 @@
                         @click.away="shouldCloseProfileMenu"
                         class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
                         aria-label="submenu">
+                        {{--
                         <li class="flex">
                             <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-customgray-200 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                                 href="#">
@@ -129,12 +130,12 @@
                                 <span>Settings</span>
                             </a>
                         </li>
+                        --}}
                         <li class="flex">
                             <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-customgray-200 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                                 href="javascript:void(0)" onclick="document.getElementById('logout-form').submit()">
                                 <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                    stroke="currentColor">
+                                    stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                     <path
                                         d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1">
                                     </path>
