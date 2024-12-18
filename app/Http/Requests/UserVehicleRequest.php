2f3267ec8d8d0e1fa332bba8ccfcd4b6420c8a45
kpebron <kpebron@gmail.com>
2024-12-18 09:17:54 +0900

    attempt to validate car as an array

--- app/Http/Requests/UserVehicleRequest.php
+++ app/Http/Requests/UserVehicleRequest.php
@@ -22,13 +22,39 @@ class UserVehicleRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+        $max_no_of_cars = 3;
+        $required_cars_rules = [];
+
+        // required or optional name and number
+        for ($i = 0; $i < $max_no_of_cars; $i++) {
+            switch ($i) {
+                case 0:
+                    // 1台目
+                    $required_cars_rules["car_name.$i"] = "required|string|max:20|required_with:car_number.$i";
+                    $required_cars_rules["car_number.$i"] = "required|string|max:20|required_with:car_name.$i";
+                    break;
+
+                default:
+                    // 2台目から
+                    $required_cars_rules["car_name.$i"] = "string|max:20|required_with:car_number.$i";
+                    $required_cars_rules["car_number.$i"] = "string|max:20|required_with:car_name.$i";
+                    break;
+            }
+        }
+
+        return array_merge([
             'user_id' => 'nullable',
-            'car_name' => 'required|string|max:20|required_with:car_number',
-            'car_katashiki' => 'nullable|string|max:20',
-            'car_number' => 'required|string|max:20|required_with:car_name',
-            'car_class' => 'nullable|string|max:30|in:' . implode(',', array_map(fn($case) => $case->value, CarClassEnum::cases())),
-        ];
+
+            // param should be an array
+            'car_name' => 'required|array|max:3',
+            'car_katashiki' => 'required|array|max:3',
+            'car_number' => 'required|array|max:3',
+            'car_class' => 'required|array|max:3',
+
+            // array element should be string
+            'car_katashiki.*' => 'string|max:20',
+            'car_class.*' => 'string|max:30|in:' . implode(',', array_map(fn($case) => $case->value, CarClassEnum::cases())),
+        ], $required_cars_rules);
     }
 
     public function attributes(): array
--- resources/views/auth/car-profile.blade.php
+++ resources/views/auth/car-profile.blade.php
@@ -20,11 +20,12 @@
 
     <!-- Car Name {{ $no }} -->
     @php
-        $key = 'car' . $no . '_name';
+        $key = 'car_name_' . $no;
+        $name = 'car_name[]';
     @endphp
     <div id="container-{{ $key }}" class="mt-4">
         <x-text.custom-input-label text="車名({{ $no }}台目)" class="mb-2" :option="$no !== 1 ? '任意' : '必須'" />
-        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $key }}"
+        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $name }}"
             :value="old($key) ?? ($userVehicles ? $userVehicles->$key : null)" :required="$no === 1 ? true : false" />
         <x-ajax-input-error id="error-{{ $key }}" class="mt-2" />
         <x-input-error :messages="$errors->get($key)" class="mt-2" />
@@ -32,11 +33,12 @@
 
     <!-- Car Katashiki {{ $no }} -->
     @php
-        $key = 'car' . $no . '_katashiki';
+        $key = 'car_katashiki_' . $no;
+        $name = 'car_katashiki[]';
     @endphp
     <div id="container-{{ $key }}" class="mt-4">
         <x-text.custom-input-label text="型式({{ $no }}台目)" class="mb-2" :option="$no !== 1 ? '任意' : '任意'" />
-        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $key }}"
+        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $name }}"
             :value="old($key) ?? ($userVehicles ? $userVehicles->$key : null)" />
         <x-ajax-input-error id="error-{{ $key }}" class="mt-2" />
         <x-input-error :messages="$errors->get($key)" class="mt-2" />
@@ -44,11 +46,12 @@
 
     <!-- Car Number {{ $no }} -->
     @php
-        $key = 'car' . $no . '_number';
+        $key = 'car_number_' . $no;
+        $name = 'car_number[]';
     @endphp
     <div id="container-{{ $key }}" class="mt-4">
         <x-text.custom-input-label text="ナンバー({{ $no }}台目)" class="mb-2" :option="$no !== 1 ? '任意' : '必須'" />
-        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $key }}"
+        <x-text-input id="{{ $key }}" class="block mt-1 w-full" type="text" name="{{ $name }}"
             :value="old($key) ?? ($userVehicles ? $userVehicles->$key : null)" :required="$no === 1 ? true : false" />
         <x-ajax-input-error id="error-{{ $key }}" class="mt-2" />
         <x-input-error :messages="$errors->get($key)" class="mt-2" />
@@ -56,14 +59,15 @@
 
     <!-- Car Class {{ $no }} -->
     @php
-        $key = 'car' . $no . '_class';
+        $key = 'car_class_' . $no;
+        $name = 'car_class[]';
     @endphp
     <div id="container-{{ $key }}" class="mt-4">
         <x-text.custom-input-label text="車種区分({{ $no }}台目)" class="mb-2" :option="$no !== 1 ? '任意' : '任意'" />
         @foreach (\App\Enums\CarClassEnum::cases() as $carClass)
             <div class="mt-4 flex items-center gap-3 mb-3">
                 <x-text-input id="{{ $key }}_{{ $carClass->value }}" type="radio"
-                    name="{{ $key }}" :value="$carClass->value" :checked="(old($key) ?? ($userVehicles ? $userVehicles->$key : null)) == $carClass->value" />
+                    name="{{ $name }}" :value="$carClass->value" :checked="(old($key) ?? ($userVehicles ? $userVehicles->$key : null)) == $carClass->value" />
                 <x-input-label for="{{ $key }}_{{ $carClass->value }}" :value="__($carClass->getLabel())" />
             </div>
         @endforeach
