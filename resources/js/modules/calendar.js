37a2a1cb282b062fff61ae85b88ab75cb2393332
kpebron <kpebron@gmail.com>
2025-01-14 19:13:00 +0900

    fix: zip code on ajax fetch

--- resources/js/modules/calendar.js
+++ resources/js/modules/calendar.js
@@ -21,11 +21,10 @@ function fontAwesomeLoaded() {
 
 // STEP06: 予約日時のクリック時
 $(document).ready(function () {
-    $.ajaxSetup({
-        headers: {
-            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-        },
-    });
+    // 郵便番号がエラー発生ので、コメントします。
+    // $.ajaxSetup({
+    //     headers: {},
+    // });
 
     // ラジオボタンが選択された時
     $('input[name="store"]').on("change", function () {
