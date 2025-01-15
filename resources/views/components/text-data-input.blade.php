b5752dc024f2550a3cd67354ad56552e614b3805
100seaocean <teruyuki.doumi@nexus-jp.co.jp>
2025-01-15 12:39:52 +0900

    時間表示修正

--- resources/views/components/text-data-input.blade.php
+++ resources/views/components/text-data-input.blade.php
@@ -1,4 +1,6 @@
 @props(['disabled' => false, 'required' => false])
 
 <input @disabled($disabled) @required($required)
-    {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm']) }}>
+    {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm']) }}
+    oninput="this.value = this.value.replace(/\u3000/g, '')"
+    >
