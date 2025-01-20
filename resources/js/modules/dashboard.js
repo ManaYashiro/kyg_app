9ee20898318a7af43b05823651e8a4b4852d3232
kpebron <kpebron@gmail.com>
2025-01-20 10:08:03 +0900

    fix: git merge code was displayed in blade file

--- resources/js/modules/dashboard.js
+++ resources/js/modules/dashboard.js
@@ -269,17 +269,4 @@ $(document).ready(function () {
                 .addClass("form-required");
         }
     }
-
-    //キーボード制御
-    // 半角英数字のみを入力できるように制限
-    $(".validateAlphanumeric").on("input", function () {
-        var value = $(this).val();
-        $(this).val(value.replace(/[^a-zA-Z0-9]/g, ""));
-    });
-
-    // 半角数字のみを入力できるように制限
-    $(".validateNumeric").on("input", function () {
-        var value = $(this).val();
-        $(this).val(value.replace(/[^0-9]/g, ""));
-    });
 });
